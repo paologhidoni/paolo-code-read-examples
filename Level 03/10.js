caa59c5108e892d9c0d83ee5262851cc176e3438
Paolo <paolo.ghidoni82@gmail.com>
2021-11-19 23:58:41 +0000

    more code reads

---
+++ Level 03/10.js
@@ -0,0 +1,68 @@
+function transform(arg1, arg2) {
+
+  return arg2.reduce((x, y) => x + arg1 + y);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ***** DANGER ZONE ********** //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(transform('-', ['Have', 'a', 'holly', 'jolly', 'Xmas']));
+// console.log(transform('-', [2, 4, 6, 8, 10, 12]));
+// console.log(transform('-', []));
